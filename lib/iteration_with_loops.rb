47d4d5e13b49b4582475f797184ce0f34793fd23
Robert Obi <robobi97@googlemail.com>
2021-02-15 04:19:52 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,11 +3,11 @@ def join_nested_strings(src)
   # Combine all Strings present in the AoA into a single value and return it
 count = 0
 sum = ""
-while count < mixed_data_1.length do
+while count < src.length do
   innercount = 0
-  while innercount < mixed_data_1[count].length do
-    if mixed_data_1[count][innercount].is_a?(String)
-      sum = sum + mixed_data_1[count][innercount]
+  while innercount < src[count].length do
+    if src[count][innercount].is_a?(String)
+      sum = sum + src[count][innercount]
     end
     innercount += 1
   end 
