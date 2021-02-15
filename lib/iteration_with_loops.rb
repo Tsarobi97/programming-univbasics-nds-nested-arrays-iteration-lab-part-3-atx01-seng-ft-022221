10516aeba32aade035adf445760f3a4d3dc83c65
Robert Obi <robobi97@googlemail.com>
2021-02-15 04:17:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,15 @@
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  count = 0
+sum = ""
+while count < mixed_data_1.length do
+  innercount = 0
+  while innercount < mixed_data_1[count].length do
+    if mixed_data_1[count][innercount].is_a?(String)
+      sum = sum + mixed_data_1[count][innercount]
+    end
+    innercount += 1
+  end 
+  count += 1
 end
